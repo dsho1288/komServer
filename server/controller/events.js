919fed7bb022cf4ff3c8948c2f7dfe46ded07011
david shostack <d.shostack@gmail.com>
2018-02-05 08:24:53 -0700

    t work

---
+++ server/controller/events.js
@@ -0,0 +1,30 @@
+/**
+ * Created by jolaadeadewale on 08/10/2017.
+ */
+import eventSchema from '../models/events';
+import sponsoredSchema from '../models/sponsored';
+
+class Event {
+
+    getEvents = (req, res) => {
+        eventSchema.find({}, (err, files) => {
+            if(err){
+
+            }
+            res.status(200).json({'file': files});
+        })
+    };
+
+    getSponsored = (req, res) => {
+
+        sponsoredSchema.find({}, (err, sponsored) => {
+            if(err){
+                console.log('err', err);
+            }
+            console.log(sponsored, 'Out here as sponsored');
+            res.status(200).json({'sponsored': sponsored});
+        })
+    }
+}
+
+export default Event;
